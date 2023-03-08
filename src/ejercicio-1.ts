7d05b8344b10a5f9824a42d7225661adfd3572c0
alu0101333281 <alu0101333281@ull.edu.es>
2023-03-08 12:57:10 +0000

    Pruebas del ejercicio-1

--- src/ejercicio-1.ts
+++ src/ejercicio-1.ts
@@ -1,181 +1,106 @@
-/**
-
-Represents a song in a music library
-*/
-interface Cancion {
-  nombre: string;
-  duracion: number;
-  generos: string[];
-  single: boolean;
-  reproducciones: number;
+interface Searchable<T> {
+  searchByYear(year: number): T[];
+  searchByName(name: string): T[];
+  searchByGenre(genre: string): T[];
 }
-/**
 
-Represents an album in a music library
-*/
-interface Disco {
-  nombre: string;
-  añoPublicacion: number;
-  canciones: Cancion[];
+interface Streamable<T> extends Searchable<T> {
+  getAll(): T[];
+  add(item: T): void;
+  remove(item: T): void;
 }
-/**
 
-Represents a music artist in a music library
-*/
-export class Artista {
-  nombre: string;
-  oyentesMensuales: number;
-  discografia: Disco[];
-  /**
+export abstract class BasicStreamableCollection<T> implements Streamable<T> {
+  protected items: T[];
 
-Constructs an instance of the Artista class
-@param nombre Name of the artist
-@param oyentesMensuales Number of monthly listeners of the artist
-@param discografia List of albums of the artist
-*/
-  constructor(nombre: string, oyentesMensuales: number, discografia: Disco[]) {
-    this.nombre = nombre;
-    this.oyentesMensuales = oyentesMensuales;
-    this.discografia = discografia;
+  constructor(items: T[] = []) {
+    this.items = items;
   }
-  /**
 
-Returns the number of songs in the specified album of the artist
-@param discoNombre Name of the album
-@returns The number of songs in the album
-@throws An error if the artist does not have an album with the specified name
-*/
-  numCancionesEnDisco(discoNombre: string): number {
-    const disco = this.discografia.find((d) => d.nombre === discoNombre);
-    if (disco) {
-      return disco.canciones.length;
-    } else {
-      throw new Error(
-        `El artista ${this.nombre} no tiene un disco llamado ${discoNombre}`
-      );
+  getAll(): T[] {
+    return this.items;
+  }
+
+  add(item: T): void {
+    this.items.push(item);
+  }
+
+  remove(item: T): void {
+    const index = this.items.indexOf(item);
+    if (index !== -1) {
+      this.items.splice(index, 1);
     }
   }
-  /**
 
-Returns the total duration in seconds of the songs in the specified album of the artist
-@param discoNombre Name of the album
-@returns The total duration in seconds of the songs in the album
-@throws An error if the artist does not have an album with the specified name
-*/
-  duracionDisco(discoNombre: string): number {
-    const disco = this.discografia.find((d) => d.nombre === discoNombre);
-    if (disco) {
-      return disco.canciones.reduce(
-        (totalDuracion, cancion) => totalDuracion + cancion.duracion,
-        0
-      );
-    } else {
-      throw new Error(
-        `El artista ${this.nombre} no tiene un disco llamado ${discoNombre}`
-      );
-    }
+  abstract searchByYear(year: number): T[];
+  abstract searchByName(name: string): T[];
+  abstract searchByGenre(genre: string): T[];
+}
+
+export class Series {
+  constructor(public name: string, public year: number, public genre: string,public seasons: number) {}
+}
+
+export class SeriesSeason {
+  constructor(public seasonNumber: number, public episodes: number) {}
+}
+
+export class SeriesCollection extends BasicStreamableCollection<Series> implements Searchable<Series> {
+  constructor(series: Series[]) {
+    super(series);
   }
-  /**
 
-Returns the total number of times the songs in the specified album of the artist have been played
-@param discoNombre Name of the album
-@returns The total number of times the songs in the album have been played
-@throws An error if the artist does not have an album with the specified name
-*/
-  reproduccionesDisco(discoNombre: string): number {
-    const disco = this.discografia.find((d) => d.nombre === discoNombre);
-    if (disco) {
-      return disco.canciones.reduce(
-        (totalReproducciones, cancion) =>
-          totalReproducciones + cancion.reproducciones,
-        0
-      );
-    } else {
-      throw new Error(
-        `El artista ${this.nombre} no tiene un disco llamado ${discoNombre}`
-      );
-    }
+  searchByYear(year: number): Series[] {
+    return this.items.filter((series) => series.year === year);
+  }
+
+  searchByName(name: string): Series[] {
+    return this.items.filter((series) => series.name.toLowerCase().includes(name.toLowerCase()));
+  }
+  searchByGenre(genre: string): Series[] {
+    return this.items.filter((series) => series.genre.includes(genre));
   }
 }
 
-export class BibliotecaMusical {
-  artistas: Artista[];
-  /**
+export class Movie {
+  constructor(public title: string, public year: number, public genre: string,public director: string) {}
+}
 
-Crea una instancia de BibliotecaMusical.
-@param artistas - Lista de artistas en la biblioteca musical.
-*/
-  constructor(artistas: Artista[]) {
-    this.artistas = artistas;
+export class MovieCollection extends BasicStreamableCollection<Movie> implements Searchable<Movie> {
+  constructor(movies: Movie[]) {
+    super(movies);
   }
-  /**
 
-Muestra información resumida de los artistas en la biblioteca musical en la consola.
-*/
-  mostrarInformacion(): void {
-    console.table(
-      this.artistas.map((a) => ({
-        Nombre: a.nombre,
-        OyentesMensuales: a.oyentesMensuales,
-        Discografia: a.discografia.map((d) => d.nombre).join(", "),
-      }))
-    );
+  searchByYear(year: number): Movie[] {
+    return this.items.filter((movie) => movie.year === year);
   }
-  /**
 
-Busca y muestra información de los artistas que coinciden con una consulta dada en la consola.
-@param query - Consulta de búsqueda.
-*/
-  buscarArtistas(query: string): void {
-    const resultados = this.artistas.filter((a) =>
-      a.nombre.toLowerCase().includes(query.toLowerCase())
-    );
-    console.table(
-      resultados.map((a) => ({
-        Nombre: a.nombre,
-        OyentesMensuales: a.oyentesMensuales,
-        Discografia: a.discografia.map((d) => d.nombre).join(", "),
-      }))
-    );
+  searchByName(name: string): Movie[] {
+    return this.items.filter((movie) => movie.title.toLowerCase().includes(name.toLowerCase()));
   }
-  /**
-
-Busca y muestra información de los discos que coinciden con una consulta dada en la consola.
-@param query - Consulta de búsqueda.
-*/
-  buscarDiscos(query: string): void {
-    const resultados = this.artistas.flatMap((a) =>
-      a.discografia
-        .filter((d) => d.nombre.toLowerCase().includes(query.toLowerCase()))
-        .map((d) => ({
-          Artista: a.nombre,
-          Disco: d.nombre,
-          AñoPublicacion: d.añoPublicacion,
-        }))
-    );
-    console.table(resultados);
+  
+  searchByGenre(genre: string): Movie[] {
+    return this.items.filter((movie) => movie.genre === genre);
   }
-  /**
+}
 
-Busca y muestra información de las canciones que coinciden con una consulta dada en la consola.
-@param query - Consulta de búsqueda.
-*/
-  buscarCanciones(query: string): void {
-    const resultados = this.artistas.flatMap((a) =>
-      a.discografia.flatMap((d) =>
-        d.canciones
-          .filter((c) => c.nombre.toLowerCase().includes(query.toLowerCase()))
-          .map((c) => ({
-            Artista: a.nombre,
-            Disco: d.nombre,
-            Cancion: c.nombre,
-            Duracion: c.duracion,
-            Generos: c.generos.join(", "),
-            Single: c.single,
-            Reproducciones: c.reproducciones,
-          }))
-      )
-    );
-    console.table(resultados);
+export class Documentary {
+  constructor(public title: string, public year: number, public genre: string, public narrator: string) {}
+}
+
+export class DocumentaryCollection extends BasicStreamableCollection<Documentary> implements Searchable<Documentary> {
+  constructor(documentaries: Documentary[]) {
+    super(documentaries);
+  }
+
+  searchByYear(year: number): Documentary[] {
+    return this.items.filter((documentary) => documentary.year === year);
+  }
+
+  searchByName(name: string): Documentary[] {
+    return this.items.filter((documentary) => documentary.title.toLowerCase().includes(name.toLowerCase()));
+  }
+  searchByGenre(genre: string): Documentary[] {
+    return this.items.filter((doc) => doc.genre.includes(genre));
   }
 }
--- test/ejercicios.spec.ts
+++ test/ejercicios.spec.ts
@@ -1,99 +1,79 @@
 import "mocha";
 import { expect } from "chai";
-import { Artista, BibliotecaMusical } from "../src/ejercicio-1";
+import { BasicStreamableCollection, Series, SeriesSeason, SeriesCollection, Movie, MovieCollection, Documentary, DocumentaryCollection } from "../src/ejercicio-1";
 import { Connect4 } from "../src/ejercicio-2";
-import { isPaseoValido } from "../src/ejercicio-1-mod";
-import { Persona, Estudiante, Profesor, Asignatura } from "../src/ejercicio-2-mod";
 
-describe("BibliotecaMusical", () => {
-  const disco1 = {
-    nombre: "Disco 1",
-    añoPublicacion: 2000,
-    canciones: [
-      {
-        nombre: "Cancion 1",
-        duracion: 120,
-        generos: ["Rock"],
-        single: true,
-        reproducciones: 100,
-      },
-      {
-        nombre: "Cancion 2",
-        duracion: 180,
-        generos: ["Pop", "Rock"],
-        single: false,
-        reproducciones: 200,
-      },
-      {
-        nombre: "Cancion 3",
-        duracion: 150,
-        generos: ["Pop"],
-        single: true,
-        reproducciones: 150,
-      },
-    ],
-  };
+describe("Streamable Collection Tests", () => {
+  describe("Series Collection", () => {
+    const series1 = new Series("Stranger Things", 2016, "Mystery", 3);
+    const series2 = new Series("Breaking Bad", 2008, "Crime", 5);
+    const series3 = new Series("The Crown", 2016, "History", 4);
+    const seriesCollection = new SeriesCollection([series1, series2, series3]);
 
-  const disco2 = {
-    nombre: "Disco 2",
-    añoPublicacion: 2010,
-    canciones: [
-      {
-        nombre: "Cancion 4",
-        duracion: 90,
-        generos: ["Pop"],
-        single: true,
-        reproducciones: 50,
-      },
-      {
-        nombre: "Cancion 5",
-        duracion: 210,
-        generos: ["Rock"],
-        single: false,
-        reproducciones: 300,
-      },
-    ],
-  };
+    it("should search by year and return the correct series", () => {
+      const result = seriesCollection.searchByYear(2016);
+      expect(result).to.deep.equal([series1, series3]);
+    });
 
-  const artista1 = new Artista("Artista 1", 1000000, [disco1]);
-  const artista2 = new Artista("Artista 2", 500000, [disco1, disco2]);
-  const biblioteca = new BibliotecaMusical([artista1, artista2]);
+    it("should search by name and return the correct series", () => {
+      const result = seriesCollection.searchByName("Stranger Things");
+      expect(result).to.deep.equal([series1]);
+    });
 
-  describe("mostrarInformacion", () => {
-    it("debería mostrar la información de los artistas en formato tabla", () => {
-      biblioteca.mostrarInformacion();
-      // Se espera que la información se muestre por consola sin errores
+    it("should search by genre and return the correct series", () => {
+      const result = seriesCollection.searchByGenre("Crime");
+      expect(result).to.be.eql([series2]);
     });
   });
 
-  /*describe('buscarArtistas', () => {
-    it('debería encontrar los artistas que coinciden con la búsqueda', () => {
-      const query = 'artista1';
-      const resultados = biblioteca.buscarArtistas(query);
-      expect(resultados[0].nombre).to.equal('Artista 1');
-      expect(resultados[1].nombre).to.equal('Artista 2');
-    });
-  });*/
-  describe("#numCancionesEnDisco", () => {
-    it("devuelve el número de canciones en un disco existente", () => {
-      const numCanciones = artista1.numCancionesEnDisco("Disco 1");
-      expect(numCanciones).to.be.eql(3);
-    });
-  });
+  describe("Movie Collection", () => {
+    const movie1 = new Movie("The Shawshank Redemption", 1994, "Drama", "Pedro");
+    const movie2 = new Movie("The Godfather", 1972, "Crime", "Juan");
+    const movie3 = new Movie("The Dark Knight", 2008, "Action", "Palomino");
+    const movieCollection = new MovieCollection([movie1, movie2, movie3]);
 
-  describe("#duracionDisco", () => {
-    it("devuelve la duración en segundos de un disco existente", () => {
-      const duracion = artista1.duracionDisco("Disco 1");
-      expect(duracion).to.be.eql(450);
+    it("should search by year and return the correct movies", () => {
+      const result = movieCollection.searchByYear(2008);
+      expect(result).to.deep.equal([movie3]);
     });
-  });
 
-  describe("#reproduccionesDisco", () => {
-    it("devuelve el número de reproducciones de un disco existente", () => {
-      const reproducciones = artista2.reproduccionesDisco("Disco 2");
-      expect(reproducciones).to.be.eql(350);
+    it("should search by name and return the correct movie", () => {
+      const result = movieCollection.searchByName("The Godfather");
+      expect(result).to.deep.equal([movie2]);
+    });
+
+    it("should search by genre and return the correct movies", () => {
+      const result = movieCollection.searchByGenre("Drama");
+      expect(result).to.be.eql([movie1]);
     });
   });
+  
+  describe('DocumentaryCollection', () => {
+    const documentaries: Documentary[] = [
+      new Documentary('Planet Earth', 2006, 'Nature', 'David Attenborough'),
+      new Documentary('The Social Dilemma', 2020, 'Social Media', 'Jeff Orlowski'),
+      new Documentary('The True Cost', 2015, 'Fashion', 'Andrew Morgan'),
+    ];
+  
+    const documentaryCollection = new DocumentaryCollection(documentaries);
+  
+    describe('searchByYear', () => {
+      it('should return documentaries released in a given year', () => {
+        const result = documentaryCollection.searchByYear(2015);
+        expect(result).to.have.lengthOf(1);
+        expect(result[0].title).to.equal('The True Cost');
+      });
+    });
+  
+    describe('searchByGenre', () => {
+      it('should return documentaries of a given genre', () => {
+        const result = documentaryCollection.searchByGenre('Nature');
+        expect(result).to.have.lengthOf(1);
+        expect(result[0].title).to.equal('Planet Earth');
+      });
+    });
+  });
+  
 });
 
 describe("Connect4", () => {
@@ -227,106 +207,5 @@ describe("Connect4", () => {
   });
 });
 
-describe("isPaseoValido function tests", () => {
-  it("isPaseoValido(['n', 'g', 's', 'e', 'o', 'n', 'n','n', 'o', 'o']), se le agrega una direccion no definida returns value undefined", () => {
-    expect(
-      isPaseoValido(["n", "g", "s", "e", "o", "n", "n", "n", "o", "o"])
-    ).to.be.eql(undefined);
-  });
-  it("isPaseoValido(['n', 'e', 's', 'e', 'o', 'n', 'n','n', 'o', 'o']), distintos en todas direcciones returns value false", () => {
-    expect(
-      isPaseoValido(["n", "e", "s", "e", "o", "n", "n", "n", "o", "o"])
-    ).to.be.eql(false);
-  });
-  it("isPaseoValido(['n', 'e', 's', 'e', 'o', 's', 'n','n', 's', 'o']), se vuelve al mismo sitio returns value true", () => {
-    expect(
-      isPaseoValido(["n", "e", "s", "e", "o", "s", "n", "n", "s", "o"])
-    ).to.be.eql(true);
-  });
-  it("isPaseoValido(['n', 'e', 's', 'e', 'o', 's', 'n','n', 's', 'o']), mayor longitud returns value false", () => {
-    expect(
-      isPaseoValido([
-        "n",
-        "e",
-        "s",
-        "e",
-        "o",
-        "s",
-        "n",
-        "n",
-        "s",
-        "o",
-        "o",
-        "e",
-      ])
-    ).to.be.eql(false);
-  });
-});
 
 
-
-
-describe('Persona', () => {
-  const persona = new Persona('John', 'Doe', '01/01/1990', 'male');
-
-  it('should allow accessing attributes through getters', () => {
-    expect(persona.getNombre()).to.equal('John');
-    expect(persona.getApellidos()).to.equal('Doe');
-    expect(persona.getFechaNacimiento()).to.equal('01/01/1990');
-    expect(persona.getGenero()).to.equal('male');
-  });
-
-  it('should allow modifying attributes through setters', () => {
-    persona.setNombre('Jane');
-    expect(persona.getNombre()).to.equal('Jane');
-
-    persona.setApellidos('Smith');
-    expect(persona.getApellidos()).to.equal('Smith');
-
-    persona.setFechaNacimiento('02/02/1990');
-    expect(persona.getFechaNacimiento()).to.equal('02/02/1990');
-
-    persona.setGenero('female');
-    expect(persona.getGenero()).to.equal('female');
-  });
-});
-
-describe('Estudiante', () => {
-  const estudiante = new Estudiante('John', 'Doe', '01/01/1990', 'male', 'john.doe@example.com');
-
-  it('should inherit from Person', () => {
-    expect(estudiante instanceof Estudiante).to.be.true;
-  });
-
-  it('should have additional attributes', () => {
-    expect(estudiante.getCorreoInstitucional()).to.equal('john.doe@example.com');
-  });
-});
-
-describe('Profesor', () => {
-  const profesor = new Profesor('Jane', 'Smith', '02/02/1990', 'female', 'jane.smith@example.com');
-
-  it('should inherit from Person', () => {
-    expect(profesor instanceof Persona).to.be.true;
-  });
-
-  it('should have additional attributes', () => {
-    expect(profesor.getCorreoInstitucional()).to.equal('jane.smith@example.com');
-  });
-});
-
-describe('Asignatura', () => {
-  const estudiante1 = new Estudiante('John', 'Doe', '01/01/1990', 'male', 'john.doe@example.com');
-  const estudiante2 = new Estudiante('Jane', 'Smith', '02/02/1990', 'female', 'jane.smith@example.com');
-  const profesor1 = new Profesor('Alice', 'Brown', '03/03/1990', 'female', 'alice.brown@example.com');
-  const profesor2 = new Profesor('Bob', 'Johnson', '04/04/1990', 'male', 'bob.johnson@example.com');
-  const asignatura = new Asignatura('Math', [profesor1, profesor2], [estudiante1, estudiante2]);
-
-  it('should have a list of students', () => {
-    expect(asignatura.getAlumnado()).to.be.eql([ 'John Doe', 'Jane Smith' ]);
-  });
-
-  it('should have a list of teachers', () => {
-    expect(asignatura.getProfesorado()).to.be.eql([ 'Alice Brown', 'Bob Johnson' ]);
-  });
-});
